f28c74b98e8bd8568bd19e3b2c0a7c38c5a5b4b1
John <johnsief12@gmail.com>
2024-04-11 12:33:32 -0400

    added value to clearing the input field and also created a function for the leads

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const btn = document.querySelector("#input-btn");
 const inputEl = document.querySelector('#input-el');
 const ulEl = document.querySelector("#ul-el");
-let myLeads = [];
 
+let myLeads = [];
 
 function saveBtn(){
 myLeads.push(inputEl.value)
-
-for (let i = 0; i < myLeads.length; i++) {
-  let li = document.createElement('li');
-  li.textContent = myLeads[i];
-  ulEl.appendChild(li);
+inputEl.value = ""
+renderLeads()
 }
 
+function renderLeads(){
+    let listItems = '';
+    for (let i = 0; i < myLeads.length; i++) {
+      listItems += '<li>' + myLeads[i] + '</li>';
+    }
+    ulEl.innerHTML = listItems;
 }
 btn.addEventListener('click', saveBtn)
