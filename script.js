b4008d1082028e0efd6d8576fcb1505a75bb1658
John <johnsief12@gmail.com>
2024-04-11 16:26:35 -0400

    added delete button and clearing out localstorage

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const btn = document.querySelector("#input-btn");
 const inputEl = document.querySelector('#input-el');
 const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
 
 let myLeads = [];
+
 
 function saveBtn(){
 myLeads.push(inputEl.value)
 inputEl.value = '';
 renderLeads()
-
+storage()
 }
+
+function storage(){
+    const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    if(leadsFromLocalStorage){
+           myLeads = leadsFromLocalStorage
+           renderLeads()
+    }
+}
+
 
 // function renderLead(){
 //     let listItem = '<li>' + inputEl.value + '</li>';
@@ -29,4 +41,13 @@ https://${myLeads[i]}
   }
     ulEl.innerHTML = listItems
 }
+
+function storageClear(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+
+}
+
 btn.addEventListener('click', saveBtn)
+deleteBtn.addEventListener('dblclick', storageClear);
